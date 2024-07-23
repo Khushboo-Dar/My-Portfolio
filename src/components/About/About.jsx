d60c16b4eb2c74319cbd93e96d2c1a87e56d2daf
Khushboo-Dar <khushboo_2022bite002@nitsri.ac.in>
2024-07-23 17:02:51 +0530

    images added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import styles from  "./About.module.css";
+import styles from "./About.module.css";
 import { getImageUrl } from '../../utils';
 export const About = () => {
-  return (
- <section className={styles.container} id="about"> 
-    <h2 className={styles.title}>About</h2>
-    <div className={styles.content}>
-        <img src={getImageUrl("about/aboutImage.png")} alt="about image" className={styles.aboutImage}/>
-        <ul className={styles.aboutItems}>
-            <li className={styles.aboutItem}><img src={getImageUrl("about/cursorIcon.png")}alt="cursor icon"></img>
-            <div className={styles.aboutItemText}>
-                <h3>Front-end Developer</h3>
-                <p>
-                    I'm a front-end developer with experience in building  optimized sites.
-                </p>
-            </div>
-            </li>
-            {/* <li className={styles.aboutItem} ><img src={getImageUrl("about/serverIcon.png")}alt="server icon"></img>
+    return (
+        <section className={styles.container} id="about">
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content}>
+                <img src={getImageUrl("about/aboutImage.png")} alt="about image" className={styles.aboutImage} />
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}><img src={getImageUrl("about/cursorIcon.png")} alt="cursor icon"></img>
+                        <div className={styles.aboutItemText}>
+                            <h3>Front-end Developer</h3>
+                            <p>
+                                As an aspiring front-end developer, I am passionate about building optimized and visually appealing websites.
+                                With a strong foundation in web technologies, I focus on creating responsive and user-friendly designs.
+                                Eager to learn and grow, I am committed to delivering efficient and engaging digital experiences.
+                            </p>
+                        </div>
+                    </li>
+                    {/* <li className={styles.aboutItem} ><img src={getImageUrl("about/serverIcon.png")}alt="server icon"></img>
             <div className={styles.aboutItemText}>
                 <h3>Back-end Developer</h3>
                 <p>
@@ -33,8 +35,8 @@ export const About = () => {
             </div>
             </li> */}
 
-        </ul>
-    </div>
- </section>
-  );
+                </ul>
+            </div>
+        </section>
+    );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,14 +7,15 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Khushboo</h1>
-        <p className={styles.description}>Aiming to leverage my expertise in web development and software engineering to
-          contribute to innovative and challenging
-        projects, enhancing business effectiveness and service quality.</p>
+        <p className={styles.description}>Aiming to leverage my expertise in web development and software
+          engineering to contribute to innovative and challenging projects,enhancing
+          business effectiveness and service quality. Currently learning and deeply interested in web development and design,
+          I am passionate about creating optimized, responsive, and visually appealing websites.</p>
         <a href="mailto:khushboodar20@gmail.com" className={styles.contactBtn}>Contact Me</a>
       </div>
-      <img src={getImageUrl("hero/heroImage.png")} alt="hero image of me" className={styles.heroImg}/>
-      <div className={styles.topBlur}/>
-      <div className={styles.bottomBlur}/>
+      <img src={getImageUrl("hero/heroImage.png")} alt="hero image of me" className={styles.heroImg} />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
 
     </section>
   );
